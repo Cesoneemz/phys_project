ba46ca4f31cc46929da672844e505addc3eb5db0
Антон <anton@Sony-VAIO>
2018-12-06 17:37:15 +0300

    include/CMakeLists.txt 	include/src/

--- include/headers/debugging_utils.h
+++ include/headers/debugging_utils.h
@@ -2,3 +2,8 @@
 #define DEBUGGING_UTILS_H
 
 #endif // DEBUGGING_UTILS_H
+#include <AL/al.h>
+#ifdef WITH_DEBUG_UTILS
+    #include "../src/debug/replay.cpp"
+#endif
+int replay(ALubyte buffer[], ALuint samples);
