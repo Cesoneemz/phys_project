8efd66f1428f1e271230815f23a5095086fe16e1
Антон <anton@Sony-VAIO>
2018-12-15 15:01:06 +0300

    удалено:       CMakeLists.txt 	удалено:       include/CMakeLists.txt 	удалено:       include/headers/debugging_utils.h 	удалено:       include/headers/main_framework.h 	удалено:       include/src/CMakeLists.txt 	удалено:       include/src/debug/CMakeLists.txt 	удалено:       include/src/debug/replay.cpp 	удалено:       include/src/record.cpp 	удалено:       main.cpp 	SoundRecognizer.cpp 	pch.cpp 	pch.h

--- include/headers/debugging_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEBUGGING_UTILS_H
-#define DEBUGGING_UTILS_H
-
-#endif // DEBUGGING_UTILS_H
-
-#ifdef WITH_DEBUG_UTILS
-    #include "../src/debug/replay.cpp"
-#endif
-int replay(ALubyte buffer[], ALuint samples);
--- include/headers/main_framework.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_FRAMEWORK_H
-#define MAIN_FRAMEWORK_H
-
-#endif // MAIN_FRAMEWORK_H
-
-int record(ALubyte *recBufptr, ALint *smpRecReturn);    // *recbufptr - указатель на массив, в который писать, *smpRecReturn - тут переменная, в которую пишется количество сэмплов
-// e.g. record(recBufptr, &smpRec);
