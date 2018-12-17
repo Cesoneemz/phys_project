bce9646d6365038b6cb00200f801255a14ee6c12
Антон <klimanov625@outlook.com>
2018-12-17 23:50:48 +0300

    изменено:      include/src/recognise.cpp

--- include/src/recognise.cpp
+++ include/src/recognise.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <cstring>
 #include "../headers/samples.h"
 
 void loadSample(const char* filename, long *length);
@@ -7,7 +8,7 @@ void determineVolume(signed char* voice_sample, int* loudest, int* quietest);
 static std::ifstream read;
 static long length;
 static short int** samples = new short int*[SAMPLES_COUNT];
-samples[0] = new short int[SAMPLE_A_LENGTH/2];
+*samples[0] = new short int[SAMPLE_A_LENGTH/2];
 
 char recognise(signed char* voice, int* smpRec) {
 	int* sample;
@@ -16,7 +17,7 @@ char recognise(signed char* voice, int* smpRec) {
 
 	return 'a';
 }
-void loadSamples() {
+int loadSamples() {
 	
 	char* filenames[SAMPLES_COUNT] { SAMPLE_A };
 	int filesizes[SAMPLES_COUNT] { SAMPLE_A_LENGTH };
@@ -24,15 +25,19 @@ void loadSamples() {
 	for (int i = 0; i < SAMPLES_COUNT; i++) {
 	    read.open(filenames[i], std::ifstream::binary);
 		if (!read.is_open()) {
-	        	printf("Error opening file %s\n", filenames[i];
+	        	printf("Error opening file %s\n", filenames[i]);
 			return -1;
 		}
 		internal_sample = new char[filesizes[i]];
 		read.read(internal_sample, filesizes[i]);
 		short int* samplePtr = samples[i];
+		char* pcm = "";
 		for (int k = 0; k < filesizes[i]; k+=2) {
-			*samplePtr = reinterpret_cast<short int>(internal_sample[k] + internal_sample[k+1]);
+			strcpy(pcm, internal_sample[k]);
+			strcpy(pcm, internal_sample[k+1]);
+			*samplePtr = reinterpret_cast<short int>(pcm);
 			samplePtr += sizeof(short int);
+			pcm = "";
 		}
 	}
 }
